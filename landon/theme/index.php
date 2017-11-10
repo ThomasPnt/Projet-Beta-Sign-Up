9f8bd487bf9879e85ef6d1a3eb0cdeefcca76ffb
Unknown <thomaspoint@gmail.com>
2017-11-10 10:37:45 +0100

    css

--- landon/theme/index.php
+++ landon/theme/index.php
@@ -1,39 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
- 
+
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="description" content="">
 	<title>LANDON - Free Bootstrap 3 Landing Page</title>
-	 
+
 	<link href="css/bootstrap.css" rel="stylesheet">
-	 
+
 	<link href="css/main.css" rel="stylesheet">
- 
+
 	<!--[if lt IE 9]>
 	  <script src="js/html5shiv.js"></script>
 	  <script src="js/respond.min.js"></script>
 	<![endif]-->
- 
+
 	<link rel="shortcut icon" href="images/favicon.png">
 	<script src="js/pace.js"></script>
- 
+
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300,600' rel='stylesheet' type='text/css'>
 </head>
 
 <body>
 	<div class="preloader"></div>
-	
-    <! -- ******************** MASTHEAD SECTION ******************** -->	
+
 	<main id="top" class="masthead" role="main">
 		<div class="container">
 			<div class="logo"> <a href="index.html#"><img src="images/logo.png" alt="logo"></a>
 			</div>
- 
-			<h1>The Most Handsome and <strong>Powerful Way</strong> <br>
+
+			<h1>Welcome at <strong>Ubeer</strong> <br>
 			to <strong>launch</strong> your product.</h1>
- 
+
 			<div class="row">
 				<div class="col-md-6 col-sm-12 col-md-offset-3 subscribe">
 					<form class="form-horizontal" role="form" action="subscribe.php" id="subscribeForm" method="POST">
@@ -42,43 +41,42 @@
 								<input class="form-control input-lg" name="email" type="email" id="address" placeholder="Enter your email" data-validate="validate(required, email)" required="required">
 							</div>
 							<div class="col-md-5 col-sm-4">
-								<button type="submit" class="btn btn-success btn-lg">GET EARLY ACCESS</button>
+								<button type="submit" class="btn btn-success btn-lg">REGISTER</button>
 							</div>
 						</div>
 					</form>
 					<span id="result" class="alertMsg"></span> </div>
 				</div>
- 
+
 				<a href="index.html#explore" class="scrollto">
 				<p>SCROLL DOWN TO EXPLORE</p>
 				<p class="scrollto--arrow"><img src="images/scroll_down.png" alt="scroll down arrow"></p>
 				</a>
 		</div><! --/container -->
 	</main><! --/main -->
- 
 
-    <! -- ******************** FEATURES SECTION ******************** -->	
+
 	<div class="container" id="explore">
- 
+
 		<div class="section-title">
-			<h2>Showcase your product with style.</h2>
-			<h4>This theme is ideal for your Web Project or your App.</h4>
+			<h2>New Mobile App</h2>
+			<h4>It offer a 24H beer delivery service anywhere in Belgium</h4>
 		</div>
 
 		<section class="row heroimg breath">
-			<div class="col-md-12 text-center"> 
-				<img src="images/flat-mockup-template.png" alt="flat-mockup-template">
+			<div class="col-md-12 text-center">
+				<img src="images/logo.png" alt="flat-mockup-template">
 			</div>
 		</section><! --/section -->
- 
+
 		<div class="section-title">
-			<h2>More information about your product.</h2>
+			<h2>More about your product</h2>
 			<h4>Always detail some important features.</h4>
 		</div>
-		
+
 		<section class="row features">
 			<div class="col-sm-6 col-md-3">
-				<div class="thumbnail"> 
+				<div class="thumbnail">
 					<img src="images/service_01.png" alt="analytics-icon">
 					<div class="caption">
 						<h3>Important Feature 1</h3>
@@ -86,9 +84,9 @@
 					</div>
 				</div><! --/thumbnail -->
 			</div><! --/col-sm-6-->
- 
+
 			<div class="col-sm-6 col-md-3">
-				<div class="thumbnail"> 
+				<div class="thumbnail">
 					<img src="images/service_02.png" alt="analytics-icon">
 					<div class="caption">
 						<h3>Important Feature 2</h3>
@@ -96,9 +94,9 @@
 					</div>
 				</div><! --/thumbnail -->
 			</div><! --/col-sm-6-->
- 
+
 			<div class="col-sm-6 col-md-3">
-				<div class="thumbnail"> 
+				<div class="thumbnail">
 					<img src="images/service_03.png" alt="analytics-icon">
 					<div class="caption">
 						<h3>Important Feature 3</h3>
@@ -106,9 +104,9 @@
 					</div>
 				</div><! --/thumbnail -->
 			</div><! --/col-sm-6-->
- 
+
 			<div class="col-sm-6 col-md-3">
-				<div class="thumbnail"> 
+				<div class="thumbnail">
 					<img src="images/service_04.png" alt="analytics-icon">
 					<div class="caption">
 						<h3>Important Feature 4</h3>
@@ -117,45 +115,43 @@
 				</div><! --/thumbnail -->
 			</div><! --/col-sm-6-->
 		</section><! --/section -->
- 
 
-	    <! -- ******************** CLIENTS SECTION ******************** -->	 
+
 		<div class="section-title">
 			<h2>Some of our clients</h2>
 			<h4>We work with different kind of brands</h4>
 		</div>
-	
+
 		<section class="row clientlogo breath">
-			<div class="col-md-12 text-center"> 
+			<div class="col-md-12 text-center">
 				<img src="images/client-logos.png" alt="client-logos">
 			</div>
 		</section><! --/section -->
- 
-	</div><! --/container -->
- 
 
-	    <! -- ******************** TESTIMONIALS SECTION ******************** -->	 
+	</div><! --/container -->
+
+
 		<div class="highlight testimonials">
 			<div class="container">
 				<div class="section-title">
 					<h2>What our customers said</h2>
 					<h4>Don't take our word. See our testimonials </h4>
 				</div>
-				
+
 				<section class="row breath">
 					<div class="col-md-6">
 						<div class="testblock">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </div>
-						<div class="clientblock"> 
+						<div class="clientblock">
 							<img src="images/customer-img-1.jpg" alt=".">
 							<p><strong>John Doe</strong> <br>
 								Founder of <a href="index.html#">Blacktie.co</a>
 							</p>
 						</div>
 					</div><! --/col-md-6 -->
-					
+
 					<div class="col-md-6">
 						<div class="testblock">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </div>
-						<div class="clientblock"> 
+						<div class="clientblock">
 							<img src="images/customer-img-2.jpg" alt=".">
 							<p><strong>Richard Sommer</strong> <br>
 								CEO of <a href="index.html#">Blacktie.co</a>
@@ -163,20 +159,19 @@
 						</div>
 					</div><! --/col-md-6 -->
 				</section><! --/section -->
- 
+
 			</div><! --/container -->
 		</div><! --/highlight Testimonials -->
- 
 
-	    <! -- ******************** PRICING LIST ******************** -->	  
+
 		<div class="container">
- 
+
 			<div class="section-title">
 				<h2>Honest pricing. No surprises.</h2>
 				<h4>No contract. No risk. No credit card required. </h4>
 			</div>
 			<section class="row breath planpricing">
-			
+
 				<div class="col-md-4">
 					<div class="pricing color1">
 						<div class="planname">Personal</div>
@@ -184,7 +179,7 @@
 						<div class="billing">Billed Annually</div>
 					</div><! --/pricing -->
 				</div><! --/col-md-4-->
-				
+
 				<div class="col-md-4">
 					<div class="pricing color2">
 						<div class="planname">Agency</div>
@@ -192,7 +187,7 @@
 						<div class="billing">Billed Annually</div>
 					</div><! --/pricing -->
 				</div><! --/col-md-4-->
-				
+
 				<div class="col-md-4">
 					<div class="pricing color3">
 						<div class="planname">Enterprise</div>
@@ -201,14 +196,13 @@
 					</div><! --/pricing -->
 				</div><! --/col-md-4-->
 			</section><!-- /section planpricing -->
- 
 
-	    <! -- ******************** FAQ ******************** -->	  
- 
+
+
 		<div class="section-title">
 			<h5>Frequently Asked Questions</h5>
 		</div>
-		
+
 		<section class="row faq breath">
 			<div class="col-md-6">
 				<h6>How does the free trial work?</h6>
@@ -216,7 +210,7 @@
 				<h6>Can I switch plans later?</h6>
 				<p>Absolutely. You can switch between our paid plans, or cancel your account altogether, whenever you like. We will adjust any payments accordingly.</p>
 			</div><! --/col-md-6 -->
-			
+
 			<div class="col-md-6">
 				<h6>Do I need to choose a plan now?</h6>
 				<p>No. You get the full featured, unlimited version of our service completely free for 10 days. Once you're ready to upgrade, you may choose a plan which suits your needs.</p>
@@ -224,23 +218,22 @@
 				<p>We accept payments from MasterCard, Visa, Visa Debit and American Express. We do not accept PayPal. Remember, you do not need to supply card details to start your free trial.</p>
 			</div><! --/col-md-6 -->
 		</section><! --/section faq -->
- 
+
 	</div><! --/container -->
- 
-	    <! -- ******************** FOOTER ******************** -->	  
+
 		<main class="footercta" role="main">
 			<div class="container">
 				<h1>The Most Simple & <strong>Powerful Way</strong> <br>
 					to <strong>launch</strong> your product</h1>
- 
+
 				<div class="row">
-					<div class="col-md-12 breath text-center"> 
-						<a href="index.html#top" class="btn btn-success btn-lg gototop">GET EARLY ACCESS</a> 
+					<div class="col-md-12 breath text-center">
+						<a href="index.html#top" class="btn btn-success btn-lg gototop">GET EARLY ACCESS</a>
 					</div>
 				</div>
 			</div><! --/container -->
 		</main><! --/main -->
- 
+
 		<div class="container">
 			<section class="row breath">
 				<div class="col-md-12 footerlinks">
@@ -248,16 +241,16 @@
 				</div>
 			</section><! --/section -->
 		</div><! --/container -->
- 
- 
- 
- 
+
+
+
+
 <script src="js/jquery.js"></script>
 <script src="js/bootstrap.js"></script>
 <script src="js/easing.js"></script>
 <script src="js/nicescroll.js"></script>
- 
- 
+
+
 <script>
 
 
@@ -270,9 +263,9 @@
      event.preventDefault();
       });
   });
-        
+
 
 </script>
- 
+
 </body>
 </html>
